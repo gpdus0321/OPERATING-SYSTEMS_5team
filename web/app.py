297e2f42da771e13680a8184199357ccd71c56c3
gpdus0321@naver.com <gpdus0321@naver.com>
2022-06-17 16:49:48 +0900

    최종

--- web/app.py
+++ web/app.py
@@ -227,6 +227,7 @@ def calc(num, TQ, __p):
         for j in range(num):
             Slist.append(Process[j])
             total += Process[j].burst
+            response_time[j] =- 1
 
         present = Slist[0]
         Slist.pop(0)
@@ -261,9 +262,6 @@ def calc(num, TQ, __p):
                 if (done_find(done, done_num, _name) == False):
                     done[done_num] = _name
                     done_num += 1
-                    avg_response += complete - burst_time[_name] - _arrive
-                    response_time[_name] = complete - \
-                        burst_time[_name] - _arrive
 
                 wait = complete - burst_time[_name] - _arrive
                 turnaround = complete - _arrive
@@ -285,6 +283,10 @@ def calc(num, TQ, __p):
                     print("asdf")
                 else:
                     break
+            if(response_time[present.name] == -1):
+                response_time[present.name] = time - present.arrive
+                avg_response += response_time[present.name]
+                
             present.burst -= 1
             time = time + 1
         avg_turnaround /= num
@@ -372,8 +374,11 @@ def calc(num, TQ, __p):
                 i = i + burst_
 
             response_time[name_] = processing_time[name_][0].start - arrive_
-            avr_response += response_time[name_]
-
+        
+        for i in range(num):
+            avr_response += response_time[i]
+            
+            
         avr_wait /= num
         avr_turnaround /= num
         avr_response /= num
